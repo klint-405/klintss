3eb0e511a3066bb1224becd4d163fc4aab2b710b
klint-405 <166080320+klint-405@users.noreply.github.com>
2024-04-05 08:20:20 +0800

    Update klint-ss.py

--- klint-ss.py
+++ klint-ss.py
@@ -211,11 +211,4 @@ def bot_share():
     except requests.exceptions.ConnectionError:
         print(f"\n{P}(!) Anda tidak terhubung ke internet!");exit()
         
-bot_share()       
-    
-
-
-
-
-
-
+bot_share()
